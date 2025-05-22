7a2bb91820230ebf1cb3790a1998e60c8ed40099
eveningwater.123 <eveningwater.123@bytedance.com>
2025-05-22 12:13:08 +0800

    feat: 修改了配置

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "@rsbuild/core";
 import { pluginVue } from "@rsbuild/plugin-vue";
 export default defineConfig({
   plugins: [pluginVue()],
-  // server: {
-  //   base: "/js-bitwise-operations/",
-  // },
+  server: {
+    base: "/js-bitwise-operations/",
+  },
   html: {
     title: "JS Bitwise Operations",
   },
